f9986768c7b8722738149fc2b767dd1d046e628f
EVA_fish <tiny.nemo.fish@gmail.com>
2021-12-24 18:38:32 +0800

    fix image path in dcm_to_png.py

--- dcm_to_png.py
+++ dcm_to_png.py
@@ -2,27 +2,30 @@ import pydicom
 import matplotlib.pyplot as plt
 import cv2
 import os
+import tqdm
 
-if not os.path.isdir(r'./imgs/train/'):
-    os.makedirs(r'./imgs/train/')
+if not os.path.isdir(r'./data/imgs/train/'):
+    os.makedirs(r'./data/imgs/train/')
 
-if not os.path.isdir(r'./imgs/test/'):
-    os.makedirs(r'./imgs/test/')
+if not os.path.isdir(r'./data/imgs/test/'):
+    os.makedirs(r'./data/imgs/test/')
 
 path = r'./data/stage_2_train_images/'
 train_list = os.listdir(path)
-for img_name in train_list:
+print({"converting dcm files in Training Set to png files"})
+for img_name in tqdm.tqdm(train_list):
     ds = pydicom.read_file(path+img_name)
     img = ds.pixel_array
-    out_path = r'./imgs/train/' + img_name[:-3] + "png"
+    out_path = r'./data/imgs/train/' + img_name[:-3] + "png"
     cv2.imwrite(out_path, img)
 
 path = r'./data/stage_2_test_images/'
 test_list = os.listdir(path)
-for img_name in test_list:
+print({"converting dcm files in Testing Set to png files"})
+for img_name in tqdm.tqdm(test_list):
     ds = pydicom.read_file(path+img_name)
     img = ds.pixel_array
-    out_path = r'./imgs/test/' + img_name[:-3] + "png"
+    out_path = r'./data/imgs/test/' + img_name[:-3] + "png"
     cv2.imwrite(out_path, img)
 
 print("finish!")
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-.PHONY: install download_dataset prepare_dataset  
+.PHONY: install download_dataset prepare_coco  
 
 install: 
 	pip install -r requirements.txt
@@ -7,3 +7,9 @@ download_dataset:
 	rm -rf data/*
 	gdown https://drive.google.com/uc?id=1ZcyAMUDBs67KU9hc4sW2DaHOVxHEPFFp -O "data/dataset.zip"
 	unzip -q "data/dataset.zip"  -d "data" 
+
+prepare_coco: 
+	rm -rf data/imgs
+	rm -f data/train.json
+	python dcm_to_png.py
+	python coco_trans.py
